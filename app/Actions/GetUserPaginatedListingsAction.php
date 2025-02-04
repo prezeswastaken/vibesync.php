8ca84a96e7a400419958d70fe998f2e51631d09c
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2025-02-04 17:59:43 +0100

    formatted eager loading realtions

--- app/Actions/GetUserPaginatedListingsAction.php
+++ app/Actions/GetUserPaginatedListingsAction.php
@@ -6,27 +6,24 @@ namespace App\Actions;
 
 use App\Models\Currency;
 use App\Models\User;
-use Illuminate\Container\Attributes\CurrentUser;
 
-class GetMyPaginatedListingsAction
+class GetUserPaginatedListingsAction
 {
     public function __construct(
         protected ConvertListingsToTargetCurrencyAction $convert,
-        #[CurrentUser] protected User $user,
     ) {}
 
-    public function handle(?Currency $currency = null)
+    public function handle(int $userId, ?Currency $currency = null)
     {
-        $listings = $this->user->listings()->with(
-            [
-                'user:id,avatar_url,name',
-                'usersWhoLiked',
-                'usersWhoDisliked',
-                'price.currency',
-                'tags',
-                'genres:id,name',
-                'links',
-            ])
+        $listings = User::findOrFail($userId)->listings()->with([
+            'user:id,avatar_url,name',
+            'usersWhoLiked',
+            'usersWhoDisliked',
+            'price.currency',
+            'tags',
+            'genres:id,name',
+            'links',
+        ])
             ->orderByDesc('created_at')
             ->paginate(10);
 
